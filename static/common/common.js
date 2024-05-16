eef24cba1be2e8ae72733bb5602ec9805c12383a
wattley <986144816@qq.com>
2024-05-16 16:29:40 +0800

    init

---
+++ static/common/common.js
@@ -0,0 +1,28 @@
+(!function (globe) {
+    "use strict";
+    globe = globe || new Object({
+        document: {}
+    });
+    globe.times = +(new Date);
+    globe.appendCode = function (temp) {
+        this.document.write(temp)
+    };
+    globe.JsToHtml = (function(){
+        let _this = this;
+        return function(temp){
+            let htmltemp = _this.document.createElement("div");
+            htmltemp.textContent != undefined ? htmltemp.textContent = temp : htmltemp.innerHTML = temp;
+            _this.document.write(htmltemp.textContent);
+            htmltemp = null;
+            return htmltemp;
+        }
+    })();
+
+
+    globe._init = function() {
+        //添加公共的css和js
+        this.appendCode(`<link rel="stylesheet" href="./static/css/index.css?v=0.0.1&t=${this.times}">`);
+        // this.appendCode(`<script src="./static/js/index.js?v=0.0.1&t=${times}"><\/script>`);
+
+    }
+})(window);
